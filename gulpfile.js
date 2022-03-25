371f722002066ecc738b3f880807a459cdc7cdd7
Daniel Flaum <talk@danielflaum.net>
2022-03-25 09:05:22 +0000

    Use PostCSS to prefix Foundation

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,39 @@
 
 var gulp = require('gulp');
 
+gulp.task('foundationCompile', function () {
+    var sass = require('gulp-sass');
+    var postcss = require('gulp-postcss');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./asset/sass/foundation/*.scss')
+        .pipe(sass({
+            outputStyle: 'nested',
+            includePaths: ['node_modules/foundation-sites/scss']
+        }).on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest('./asset/precss'));
+});
+
+gulp.task('foundationPrefix', function () {
+    var postcss = require('gulp-postcss');
+    var prefixer = require('postcss-prefixer');
+
+    var processors = [prefixer({ prefix: 'f-' })];
+
+    return gulp.src('./asset/precss/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./asset/css'));
+});
+
 gulp.task('css', function () {
     var sass = require('gulp-sass');
     var postcss = require('gulp-postcss');
     var autoprefixer = require('autoprefixer');
 
-    return gulp.src('./asset/sass/*.scss')
+    return gulp.src('./asset/sass/theme/*.scss')
         .pipe(sass({
             outputStyle: 'nested',
             includePaths: ['node_modules/foundation-sites/scss']
